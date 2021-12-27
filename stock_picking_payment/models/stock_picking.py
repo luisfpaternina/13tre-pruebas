39e4c74881283206e1db8c521f89cb6551d73f91
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-12-27 10:56:46 -0500

    add

--- stock_picking_payment/models/stock_picking.py
+++ stock_picking_payment/models/stock_picking.py
@@ -3,6 +3,3 @@ from odoo import models, fields, api, _
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-    
-    estado_factura = fields.Char(related="sale_id.invoice_ids[0].invoice_payment_state", string="Estado Factura")    
-    factura = fields.Char(related="sale_id.invoice_ids[0].display_name", string="Factura")
