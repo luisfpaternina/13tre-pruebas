d6cdd8f9407eec8bbfd328d3fee37f1d437cb4a3
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-12-27 11:17:43 -0500

    Update stock_picking.py

--- stock_picking_payment/models/stock_picking.py
+++ stock_picking_payment/models/stock_picking.py
@@ -3,3 +3,7 @@ from odoo import models, fields, api, _
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+    invoice_name = fields.Char(
+        string="Invoice",
+        related="sale_id.invoice_ids.display_name")
