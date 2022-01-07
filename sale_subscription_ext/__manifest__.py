2f5d79527f0e7c92fe91695353838f6cfc0cfe3c
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-07 10:52:37 -0500

    add

--- sale_subscription_ext/__manifest__.py
+++ sale_subscription_ext/__manifest__.py
@@ -1,39 +1,29 @@
 {
-    'name': 'SAT COMPANIES',
+    'name': 'SALE SUBSCRIPTION EXT',
 
-    'version': '14.0.1',
+    'version': '13.0.1.0',
 
-    'author': "Process Control",
+    'author': "Nybble group",
 
     'contributors': ['Luis Felipe Paternina'],
 
-    'website': "https://www.processcontrol.es/",
+    'website': "",
 
-    'category': 'Maintenance',
+    'category': 'Sale',
 
     'depends': [
 
         'sale_management',
-        'contacts',
-        'account_accountant',
-        'account',
         'sale_subscription',
-        'crm',
-        'base',
-        'base_automation',
-        'hr_holidays'
 
     ],
 
     'data': [
        
-        'security/security.xml',
-        'security/ir.model.access.csv',
+        #'security/security.xml',
+        #'security/ir.model.access.csv',
         #'views/account_move.xml',
-        'views/res_company.xml',
-        'views/account_move_line.xml',
                    
     ],
     'installable': True
 }
-
