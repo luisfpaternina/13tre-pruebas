aa582c9dd2bc2ce50529d36bb41124a8509faf56
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-07 15:25:00 -0500

    add

---
+++ administrative_expenses/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Administrative expenses',
+
+    'version': '13.0.1.0',
+
+    'author': "Nybble group",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "",
+
+    'category': 'Sale',
+
+    'depends': [
+
+        'account_accountant',
+
+    ],
+
+    'data': [
+       
+        #'views/sale_subscription.xml',
+                   
+    ],
+    'installable': True
+}
