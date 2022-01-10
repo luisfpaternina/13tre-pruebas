2977a736df415c898c0c496ab420076545fea4c0
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-10 13:56:32 -0500

    add

--- sale_subscription_ext/models/sale_subscription.py
+++ sale_subscription_ext/models/sale_subscription.py
@@ -4,4 +4,10 @@ from odoo import models, fields, api, _
 class SaleSubscription(models.Model):
     _inherit = 'sale.subscription'
 
-    city = fields.Char(string="City",related="partner_id.city")
+    city = fields.Char(string="City")
+
+    @api.onchange('partner_id')
+    def onchange_partner(self):
+        for record in self:
+            if record.partner_id:
+                record.city = record.partner_id.city
