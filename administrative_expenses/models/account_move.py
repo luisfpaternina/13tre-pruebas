8462c37d2422daa230af69b7a83eb17c9f81db9e
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-11 11:26:09 -0500

    add

--- administrative_expenses/models/account_move.py
+++ administrative_expenses/models/account_move.py
@@ -1,4 +1,11 @@
 from odoo import models, fields, api, _
+import logging
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
+    def _validate_subscription(self):
+        for record in self:
+            sale_obj = record.env['sale.order'].search([('name', '=', record.invoice_origin)])
+            if sale_obj:
+                logging.info('######################################', sale_obj)
