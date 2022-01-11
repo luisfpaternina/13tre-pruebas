34cd1c81ca225bbff4adaf32983b9a70b7434774
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-11 12:30:37 -0500

    Update account_move.py

--- administrative_expenses/models/account_move.py
+++ administrative_expenses/models/account_move.py
@@ -7,13 +7,21 @@ class AccountMove(models.Model):
     is_validate = fields.Boolean(string="Validate")
 
 
+    def create_records(self,rec):
+        dic = {
+        'name': rec.name,
+        }
+        self.env['sale.subscription'].create(dic)
+
+
     # @api.onchange('is_validate')
     def _validate_subscription(self):
         for record in self:
             sale_obj = record.env['sale.order'].search([('name', '=', record.invoice_origin)])
             if sale_obj:
-                logging.info('######################################')
                 record.is_validate = True
+                if record.invoice_date > record.invoice_date_due:
+                    record.create_records()
             else:
                 record.is_validate = False
 
