bf8c37c13d0784918f537d6c8ae7cca312e22cfa
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-12 12:20:31 -0500

    Update sale_order.py

--- administrative_expenses/models/sale_order.py
+++ administrative_expenses/models/sale_order.py
@@ -4,26 +4,8 @@ import logging
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    def _generate_lines(self):
-        for record in self:
-            record.order_line._validate_sale_subscription()
-
     
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-
-
-    def _validate_sale_subscription(self):
-        for record in self:
-            subscription_obj = record.env['sale.subscription'].search([('display_name', '=', record.subscription_id.name)])
-            if subscription_obj:
-                subscription = self.env['sale.subscription'].create({
-                    'recurring_invoice_line_ids': [(0, 0, {
-                    'product_id': line.product_id.id,
-                    'quantity': 1,
-                    'price_unit': line.price_unit
-                    }) for line in record.order_line]
-                })
-                return subscription
