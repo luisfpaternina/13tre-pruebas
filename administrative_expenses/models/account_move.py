4ff2534cea653ac1b9010dd65c3c8eeda220aac6
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-12 10:23:37 -0500

    Update account_move.py

--- administrative_expenses/models/account_move.py
+++ administrative_expenses/models/account_move.py
@@ -40,12 +40,4 @@ class AccountMove(models.Model):
                     record.is_validate = False
 
 
-    def action_post(self):
-        rec = super(AccountMove, self).action_post()
-        if self.is_validate:
-            logging.info("VALIDAR....!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            if self.invoice_date < self.invoice_date_due:
-                logging.info("FECHAS....!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                self._validate_subscription()
-                logging.info("19999999999999999999999999999999999999999999993333333333")     
-            return rec
+    
