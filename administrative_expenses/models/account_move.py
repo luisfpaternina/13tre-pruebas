1e74d8b474f3806a7023c7a301625a9657306262
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-12 14:51:26 -0500

    Update account_move.py

--- administrative_expenses/models/account_move.py
+++ administrative_expenses/models/account_move.py
@@ -37,30 +37,31 @@ class AccountMove(models.Model):
 
     def _validate_subscription(self):
         for record in self:
-            sale_obj = record.env['sale.order'].search([('name', '=', record.invoice_origin)])
-            subscription_obj = record.env['sale.subscription'].search([])
-            logging.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
-            logging.info(subscription_obj)
-            for s in subscription_obj:
-                if sale_obj:
-                    record.is_validate = True
-                    if s in sale_obj.order_line.subscription_id:
-                        s.display_name
-                        logging.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-                        logging.info(s.display_name)
-                        vals = {
-                        'partner_id': s.partner_id.id,
-                        'recurring_invoice_line_ids': [(0, 0, {
-                            'product_id': s.recurring_invoice_line_ids.product_id.id,
-                            'name': 'Cargo por pago fuera de término',
-                            'price_unit': 2500,
-                            'quantity': 1,
-                            'uom_id': s.recurring_invoice_line_ids.uom_id.id,
-                            })]
-                        }
-                        logging.info("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
-                        logging.info(vals)
-                        s.write(vals)
-                        break
-                else:
-                    record.is_validate = False
+            if record.state == 'posted':
+                sale_obj = record.env['sale.order'].search([('name', '=', record.invoice_origin)])
+                subscription_obj = record.env['sale.subscription'].search([])
+                logging.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+                logging.info(subscription_obj)
+                for s in subscription_obj:
+                    if sale_obj:
+                        record.is_validate = True
+                        if s in sale_obj.order_line.subscription_id:
+                            s.display_name
+                            logging.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+                            logging.info(s.display_name)
+                            vals = {
+                            'partner_id': s.partner_id.id,
+                            'recurring_invoice_line_ids': [(0, 0, {
+                                'product_id': s.recurring_invoice_line_ids.product_id.id,
+                                'name': 'Cargo por pago fuera de término',
+                                'price_unit': 2500,
+                                'quantity': 1,
+                                'uom_id': s.recurring_invoice_line_ids.uom_id.id,
+                                })]
+                            }
+                            logging.info("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
+                            logging.info(vals)
+                            s.write(vals)
+                            break
+                    else:
+                        record.is_validate = False
